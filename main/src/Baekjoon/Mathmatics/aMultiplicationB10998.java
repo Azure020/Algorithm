8b8fffc8edb513af1610152dc739a7dcff5ac97a
hwangmingyeong <miniky04@naver.com>
2022-08-19 22:24:30 +0900

    java baekjoon bronze5 mathmatics aMultiplicationB10998 bettercode

---
+++ main/src/Baekjoon/Mathmatics/aMultiplicationB10998.java
@@ -0,0 +1,22 @@
+package Baekjoon.Mathmatics;
+
+import java.io.IOException;
+// import java.util.Scanner;
+
+public class aMultiplicationB10998 {
+    public static void main(String[] args) throws IOException {
+        int a = System.in.read();
+        System.in.read();
+        int b = System.in.read();
+        System.out.println((a-48)*(b-48));
+    }
+}
+
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//
+//        int num1 = sc.nextInt();
+//        int num2 = sc.nextInt();
+//
+//        System.out.println(num1 * num2);
+//    }

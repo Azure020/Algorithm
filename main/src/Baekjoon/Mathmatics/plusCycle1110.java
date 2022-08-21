926e3db42ac0d203b35eb23e2c59279eba3d6389
hwangmingyeong <miniky04@naver.com>
2022-08-21 17:11:00 +0900

    java baekjoon bronze1 mathmatics plusCycle1110

---
+++ main/src/Baekjoon/Mathmatics/plusCycle1110.java
@@ -0,0 +1,26 @@
+package Baekjoon.Mathmatics;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class plusCycle1110 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(br.readLine());
+        int count = 0;
+        int a = num;
+
+        while(true) {
+            int left = a / 10, right = a % 10;
+            int sum = left + right;
+
+            a = right * 10 + sum % 10;
+            count++;
+
+            if (a == num) break;
+        }
+        System.out.println(count);
+    }
+}

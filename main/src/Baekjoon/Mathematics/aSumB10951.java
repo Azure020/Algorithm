f3297da7122ca6b34000ff9c21470cabb9dbba88
hwangmingyeong <miniky04@naver.com>
2022-08-23 12:44:10 +0900

    java baekjoon bronze5 mathematics aSumB10951

---
+++ main/src/Baekjoon/Mathematics/aSumB10951.java
@@ -0,0 +1,15 @@
+package Baekjoon.Mathematics;
+
+import java.util.Scanner;
+
+public class aSumB10951 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while(sc.hasNext()) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(a+b);
+        }
+    }
+}

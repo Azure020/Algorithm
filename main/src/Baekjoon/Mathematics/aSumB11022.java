9ca67cccef34e5ea44c22f4e1d0f0c0f66951271
hwangmingyeong <miniky04@naver.com>
2022-08-23 12:40:05 +0900

    java baekjoon bronze5 mathematics aSumB11022

---
+++ main/src/Baekjoon/Mathematics/aSumB11022.java
@@ -0,0 +1,22 @@
+package Baekjoon.Mathematics;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class aSumB11022 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+
+        for(int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            System.out.println("Case #"+i+": "+a+" + "+b+" = "+(a+b));
+        }
+    }
+}
